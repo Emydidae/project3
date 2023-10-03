42ac006df7b9a21b9f2e9fa346e3dbb25ff82a74
Emydidae <peterjsolis@gmail.com>
2023-10-03 19:51:40 -0400

    added select menu functionality and some formatting / info

--- 05 - HTML & JavaScript/static/js/logic.js
+++ 05 - HTML & JavaScript/static/js/logic.js
@@ -1,8 +1,10 @@
 // use these to test the different endpoints
 // d3.json('http://127.0.0.1:5000/map_markers/2017').then(data => {console.log(data);});
 // d3.json('http://127.0.0.1:5000/rain_bar/2017').then(data => {console.log(data);});
-d3.json('http://127.0.0.1:5000/fire_bar/2017').then(data => {console.log(data);});
+// d3.json('http://127.0.0.1:5000/fire_bar/2017').then(data => {console.log(data);});
 // d3.json('http://127.0.0.1:5000/month_line').then(data => {console.log(data);});
+
+// MAPS ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // background layer of maps
 function make_tiles() {
@@ -25,12 +27,12 @@ function make_tiles() {
 };
 
 function rain_color(d) {
-    return d > 200 ? '#084594':
-           d > 175 ? '#2171B5':
-           d > 150 ? '#4292C6':
-           d > 125 ? '#6BAED6':
-           d > 100 ? '#9ECAE1':
-           d > 75  ? '#C6DBEF':
+    return d > 150 ? '#084594':
+           d > 125 ? '#2171B5':
+           d > 100 ? '#4292C6':
+           d > 75 ? '#6BAED6':
+           d > 50 ? '#9ECAE1':
+           d > 25  ? '#C6DBEF':
            d > 0   ? '#EFF3FF':
                      '#000000';
 }
@@ -45,16 +47,16 @@ function rain_markers(rain_data) {
         let popup_text = `<h3>${rain_data[i].Station_ID} - ${rain_data[i].Station_Name}</h3>\
 <hr><p>Latitude: ${rain_data[i].Latitude}<br>
 Longitude: ${rain_data[i].Longitude}<br>
-Total Pcpn (in): ${rain_data[i].total_pcpn_inches}<br>
-Total Expected Pcpn: ${rain_data[i].total_expected_pcpn}<br>
-% of Expected: ${rain_data[i].percent_avg_pcpn}<br>
+Total Pcpn: ${rain_data[i].total_pcpn_inches} in.<br>
+Total Expected (Avg) Pcpn: ${rain_data[i].total_expected_pcpn} in.<br>
+% of Expected: ${rain_data[i].percent_avg_pcpn}%<br>
 # Months Measured: ${rain_data[i].months_measured}<br>
 <a href = '${rain_data[i].info_link}'>More Info</a></p>`;
         // create marker, add popup text to it
         let marker = L.circle([rain_data[i].Latitude, rain_data[i].Longitude], {
             color: 'black',
             // size = amount prcp
-            radius: rain_data[i].total_pcpn_inches * 100,
+            radius: rain_data[i].total_pcpn_inches * 200,
             // color = % avg
             fillColor: rain_color(rain_data[i].percent_avg_pcpn),
             fillOpacity: .75,
@@ -85,7 +87,7 @@ Year: ${fire_data[i].Year}<br></p>`;
         let marker = L.circle([fire_data[i].Latitude, fire_data[i].Longitude], {
             color: 'black',
             // size = acres burned
-            radius : fire_data[i].Acres_Burned,
+            radius: Math.sqrt(fire_data[i].Acres_Burned) * 150,
             fillColor: '#d94801',
             fillOpacity: 0.75,
             opacity: 1,
@@ -101,6 +103,11 @@ Year: ${fire_data[i].Year}<br></p>`;
 
 // make the rain map chart
 function make_maps(year) {
+    // remove current maps & re-insert divs
+    d3.select('#rain_map').remove();
+    d3.select('#fire_map').remove();
+    d3.select('#map_holder').append('div').attr('id','rain_map');
+    d3.select('#map_holder').append('div').attr('id','fire_map');
     // make url & call for map marker data
     let url = `http://127.0.0.1:5000/map_markers/${year}`
     d3.json(url).then(data => {
@@ -144,6 +151,8 @@ function make_maps(year) {
     });
 };
 
+// BARS ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // make the rain bar chart
 function make_rain_bar(year) {
     // make url & call for data
@@ -154,11 +163,14 @@ function make_rain_bar(year) {
             x: data.map(item => {return item.month}),
             y: data.map(item => {return item.avg_rainfall}),
             text: data.map(item => {return `Inches`}),
-            type: 'bar'
+            type: 'bar',
+            marker: {
+              color: '#2171B5'
+            }
         }];
         // give name
         let layout = {
-            title: `Monthly Average Precipitation in California - ${year}`
+            title: `Monthly Average Precipitation in CA - ${year}`
         };
         // make chart
         Plotly.newPlot('rain_bar', trace1, layout);
@@ -166,7 +178,7 @@ function make_rain_bar(year) {
 };
 
 // make the fire bar chart
-function make_fire_bar(year) {
+function make_fire_bars(year) {
     // make url & call for data
     let url = `http://127.0.0.1:5000/fire_bar/${year}`
         // make trace with x being the month and 2 different y axes - avg acres burned and number of fires
@@ -174,48 +186,73 @@ function make_fire_bar(year) {
                 // https://plotly.com/javascript/multiple-axes/ - some resources on how to do multiple bars
     d3.json(url).then(data => {
         // make trace for y axis 1
-        let trace1 = [{
+
+        let trace1 = {
             x: data.map(item => {return item.month}),
             y: data.map(item => {return item.avg_acres_burned}),
-            text: data.map(item => {return `Average Acres Burned`}),
-            type: 'bar'
-        }];
-        
-        // make trace for y axis 2
-        let trace2 = [{
+            text: data.map(item => {return 'Average Acres Burned'}),
+            type: 'bar',
+            marker: {
+              color: '#d94801'
+            }
+        };
+          
+        let trace2 = {
             x: data.map(item => {return item.month}),
             y: data.map(item => {return item.num_of_fires}),
-            text: data.map(item => {return `Number of Fires`}),
-            type: 'bar'
-        }];
-
-        // give name
-        let layout = {
-            title: `Monthly Fire Statistics in California - ${year}`,
-            yaxis: {title: 'Acres Burned'},
-            yaxis2: {title: 'Number of Fires',
-                    side: 'right'}
+            name: 'Fires',
+            type: 'bar',
+            marker: {
+              color: '#d94801'
+            }
         };
-
-        // make chart
-        Plotly.newPlot('rain_bar', [trace1, trace2], layout);
+          
+        let layout = {
+            title: `Monthly Average Acres Burned in CA - ${year}`
+        };
+          
+        let layout2 = {
+            title: `Monthly Number of Fires in CA - ${year}`
+        };
+          
+        Plotly.newPlot('fire_bar1', [trace1], layout);
+        Plotly.newPlot('fire_bar2', [trace2], layout2);
     });
 };
 
-function make_line() {
+// LINE ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+function make_line() {
+    console.log('hi')
 };
+
+// DROPDOWN FUNCTIONALITY ----------------------------------------------------------------------------------------------------------------------------------------------------
 
 // update the visuals when a different year is selected
-function init_page(year) {
+function update_vis() {
+    let year = Number(d3.select('select').property('value'));
+    d3.select('#rain_title').text(`${year} Rainy Season Data (November - June)`);
+    d3.select('#fire_title').text(`${year} Fire Season Data (June - November)`);
     make_maps(year);
     make_rain_bar(year);
-    make_fire_bar(year);
+    make_fire_bars(year);
 };
 
-// call function to make the visuals
-//init_page(2017); // delete this when dropdown is implemented !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//make_line();
-make_maps(2017);
-make_rain_bar(2017);
-make_fire_bar(2017);
+// get select menu
+let select_menu = d3.select('#sel_year');
+
+// read in samples.json to the dropdown
+d3.json('http://127.0.0.1:5000/years').then(data => {
+    for (let i = 0; i < data.length; i++) {
+        select_menu.append('option').attr('value', data[i]).text(data[i]);
+    };
+    // call function to initialize the visuals
+    update_vis();
+});
+
+// initialize line
+make_line();
+
+// Call update_vis() when a change takes place
+d3.select("#sel_year").on("change", update_vis);
+
